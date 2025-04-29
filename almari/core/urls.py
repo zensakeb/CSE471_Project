0f4b5510c6857c15556fe3d11a9593b5425a423f
Sakeb <md.sakib.khan1@g.bracu.ac.bd>
2025-04-29 23:38:44 +0600

    Don't need core anymore

--- almari/core/urls.py
+++
@@ -1,8 +0,0 @@
-# core/urls.py
-from django.urls import path
-from .views import frontpage
-
-urlpatterns = [
-    path('', frontpage, name='frontpage'),  # This makes the URL for frontpage view
-]
-
