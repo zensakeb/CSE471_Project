fbbb16de1f29b5164489441dfa5780ea43ea2271
Sakeb <md.sakib.khan1@g.bracu.ac.bd>
2025-05-05 00:12:00 +0600

    minor changes

--- almari/almari/wsgi.py
+++ almari/almari/wsgi.py
@@ -13,7 +13,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'almari.almari.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'almari.settings')
 
 application = get_wsgi_application()
 print(os.environ.get('DJANGO_SETTINGS_MODULE'))
